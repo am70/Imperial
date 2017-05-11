c6158e03da905713916fb7a1b24b4a64abffa442
am70 <aaron.morris10@imperial.ac.uk>
2017-05-11 09:19:26 +0100

    startup update

--- larvalModel/packages/odinPackage/startup.R
+++ larvalModel/packages/odinPackage/startup.R
@@ -25,7 +25,7 @@ rainfall<-subset(rainfall, date >= as.Date("1973-05-27") & date <= as.Date("1974
 rainfall$time<-(1:nrow(rainfall))
 
 #load in mosquito data
-garki<-read.table("Q:\\ImperialMalaria\\larvalModel\\Data\\spraycollect.csv",sep=",",head=T)
+garki<-read.table("Q:\\Imperial\\larvalModel\\Data\\spraycollect.csv",sep=",",head=T)
 garki$ag_sum<-rowSums(garki[,c(8:16)])#create sum of A.gambiae spray samples
 garki$Date<-as.Date(garki$Date)
 #garki$Date<-dmy(as.character(garki$Date))
@@ -62,7 +62,7 @@ clusterEvalQ(cl, library("odinPackage"))
 clusterEvalQ(cl, library("lhs"))
 clusterEvalQ(cl, library("VGAM"))
 clusterEvalQ(cl, library("deSolve"))
-clusterExport(cl, c("rFx","delta","garkiObs","data.point"), envir=environment())
+clusterExport(cl, c("rFx","delta","garkiObs"), envir=environment())
 
 
 #########################################################################################################################################
@@ -79,110 +79,39 @@ obj$cluster_load(TRUE)
 sources<-c("Q:\\Imperial\\larvalModel\\packages\\odinPackage\\data_functions.R")
 
 ctx <- context::context_save(root,
-                             package_source=provisionr::package_sources(github=c("richfitz/odin","richfitz/dde"),
+                             package_source=provisionr::package_sources(
                              local="Q:\\Imperial\\larvalModel\\packages\\odinPackage"),
                              sources=sources,
-                             packages = c("lhs","VGAM","deSolve","odin","lubridate","odinPackage","dde"))
+                             packages = c("lhs","VGAM","deSolve","lubridate","odinPackage","dde","buildr"))
 
-obj <- didehpc::queue_didehpc(ctx,didehpc::didehpc_config(cluster="mrc",cores = 16,home="//fi--san02/homes/alm210"))
-
-
-
-#################################particle filter tests###################################
-
-p1<-obj$enqueue(parRunS(1:16,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k1")
-p2<-obj$enqueue(parRunS(17:32,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k2")
-p3<-obj$enqueue(parRunS(33:48,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k3")
-p4<-obj$enqueue(parRunS(49:64,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k4")
-p5<-obj$enqueue(parRunS(65:80,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k5")
-p6<-obj$enqueue(parRunS(81:96,500000,seq(0.002, 0.002, length.out = 500000),"uoE"),name="particle filter 100k6")
-pRes100k<-c(unlist(p1$result()),unlist(p2$result()),unlist(p3$result()),unlist(p4$result()),unlist(p5$result()),unlist(p6$result()))
+obj <- didehpc::queue_didehpc(ctx,didehpc::didehpc_config(cluster="mrc",home="//fi--san02/homes/alm210",cores=16,parallel = T))
 
 
-puoE2<-obj$enqueue(parRunS(1:500,1000,seq(0.002, 0.002, length.out = 100),"uoE"),name="particle filter uoE")
-puoL<-obj$enqueue(parRunS(1:16,2000,seq(0.01, 0.01, length.out = 16),"uoL"),name="particle filter uoL")
-puP<-obj$enqueue(parRunS(1:1000,5000,seq(0.001, 0.5, length.out = 1000),"uP"),name="particle filter uP")
-pn<-obj$enqueue(parRunS(1:500,5000,seq(0, 20, length.out = 500),"n"),name="particle filter n")
-psf<-obj$enqueue(parRunS(1:500,5000,seq(0, 20, length.out = 500),"sf"),name="particle filter sf")
-pY<-obj$enqueue(parRunS(1:500,5000,seq(0, 20, length.out = 500),"y"),name="particle filter Y")
+#obj <- didehpc::queue_didehpc(ctx,didehpc::didehpc_config(cluster="dide",home="//fi--san02/homes/alm210",use_rrq =TRUE))
+
+
+
+
+#Submit workers
+obj$submit_workers(80)
+
+
+
+p1<-obj$enqueue(parRunWorker(1:100,1,seq(1, 1.5, length.out = 100),"n"),name="particle filter X")
+
 
 
 ##################particle filter MCMC tester#######################################
-
-particleFilterMCMC(fitmodel=larvalModP, 
-                   theta=fit.params,
+f<-NULL
+for(i in 1:100){
+s<-odinPackage::particleFilter(fitmodel=larvalModP, 
+                   theta=theta(),
                    init.state = init.state,
                    data = garkiObs,
-                   n.particles = 200000)
+                   nParticles = 100)
+f<-rbind(f,s)
+print(s)
 
-
-###########################plots######################################
-
-puoEplot<-ggplot()+
-  xlab("uoE")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(puoE$result()), x=seq(0.001, 0.1, length.out = 500)), color='red',alpha=0.9)+
-  theme_bw()
-
-
-puoLplot<-ggplot()+
-  xlab("uoL")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(puoL$result()), x=seq(0.001, 0.1, length.out = 500)), color='red',alpha=0.9)+
-  theme_bw()
-
-puPplot<-ggplot()+
-  xlab("uP")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(puP$result()), x=seq(0.001, 0.3, length.out = 1000)), color='red',alpha=0.9)+
-  theme_bw()
-
-
-psfplot<-ggplot()+
-  xlab("sf")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(psf$result()), x=seq(0, 20, length.out = 500)), color='red',alpha=0.9)+
-  theme_bw()
-
-pnplot<-ggplot()+
-  xlab("n")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(pn), x=seq(0, 20, length.out = 500)), color='red',alpha=0.9)+
-  theme_bw()
-
-pYplot<-ggplot()+
-  xlab("Y")+
-  ylab("Marginal log Like")+
-  geom_point(aes(y=unlist(pY$result()), x=seq(0, 20, length.out = 500)), color='red',alpha=0.9)+
-  theme_bw()
-
-
-resAllplot<-ggplot()+
-  xlab("particles")+
-  ylab("S.D.")+
-  geom_point(aes(y=resAllMSD$sSD, x=resAllMSD$i), color='red',alpha=0.9)+
-  geom_line(aes(y=resAllMSD$sSD, x=resAllMSD$i), color='red',alpha=0.9)+
-  theme_bw()
-
-#(sf*(1/(trx*(1-exp(-step/trx)))))*(sum(rF[(step-trx):step]))
-#(sf*((1/trx)*(sum(rF[(step-trx):step]))))
-
-
-resAll<-NULL
-resAll<-rbind(p1kR,p2kR,p3kR,p4kR,p5kR,p6kR,p7kR,p8kR,p9kR,
-              pRes10k,pRes20k,pRes30k,pRes40k,pRes50k,pRes100k,pRes200k,pRes1Mil)
-
-
-resAllMSD<-NULL
-for (i in unique(resAll$particles)){
-  
-  s<-subset(resAll,particles==i)
-  sMean<-mean(s$ll)
-  sSD<-sd(s$ll)
-  d<-cbind(sMean,sSD,i)
-  resAllMSD<-rbind(resAllMSD,d)
 }
-
-
 
 
