8c1bda18d6a42d51c5f10903a567c35ab1c1fcab
am70 <aaron.morris10@imperial.ac.uk>
2017-05-31 16:10:43 +0100

    critical changes

--- larvalModel/packages/odinPackage/R/particleFilter.R
+++ larvalModel/packages/odinPackage/R/particleFilter.R
@@ -1,14 +1,5 @@
 
-
-
-# define a sampler for the prior on the initial state
-simx0 <- function(N,t0)
-{
-  mat=cbind(rpois(N,177),rpois(N,8),rpois(N,1),rpois(N,7))
-  colnames(mat)=c("E","L","P","M")
-  mat
-}
-
+##Zero inflated poisson density function
 ziP<-function(x,lambda,p0,log=FALSE){
   if(log==FALSE){
     ifelse(x==0,p0+(1-p0)*dpois(x,lambda),(1-p0)*dpois(x,lambda))
@@ -18,40 +9,51 @@ ziP<-function(x,lambda,p0,log=FALSE){
   }
 }
 
+
+# define a sampler for the prior on the initial state
+iState <- function(N,t0)
+{
+  mat=cbind(rpois(N,177),rpois(N,8),rpois(N,1),rpois(N,7))
+  colnames(mat)=c("E","L","P","M")
+  mat
+}
+
+
 dataLik2 <- function(input)
 {
   dataInput<-read.table(text = input, sep = ",", colClasses = "numeric")
-  ll = ziP(dataInput[2], dataInput[1], pstr0=0, log = T)
+  ll = dzipois(dataInput[2], dataInput[1], pstr0=0, log = T)
   return (exp(ll))
 }
 
+
 ###Particle Filter
-pFilt <- function (n, simx0, t0, stepFun, dataLik, data,pr) 
+pFilt <- function (n, iState, t0, stepFun, dataLik, obsData,prms) 
 {
-  times = c(as.numeric(data$time))
-  xmat = simx0(n, t0) #initial state
+  times = c(as.numeric(obsData$time))
+  particles = iState(n, t0) #initial state
   ll = 0
   for (i in 1:length(times[-length(times)])) {
-    wp<-paste(xmat[,1],xmat[,2],xmat[,3], xmat[,4],times[i],
-              times[i + 1],pr[1],pr[2],pr[3],pr[4],pr[5],pr[6],sep=",")
+    wp<-paste(particles[,1],particles[,2],particles[,3], particles[,4],times[i],
+              times[i + 1],prms[1],prms[2],prms[3],prms[4],prms[5],prms[6],sep=",")
     
-    xmatTemp = parLapply(NULL,wp,stepFun)
-    xmat<-data.frame(t(sapply(xmatTemp, `[`)))
+    particlesTemp = parLapply(cl,wp,stepFun) #use NULL for dide cluster, cl for local
+    particles<-data.frame(t(sapply(particlesTemp, `[`)))
     
-    likeDat<-paste(xmat$M,times[i],sep=",")
-    w = lapply(likeDat,dataLik)
-    w<-as.vector(unlist(w))
+    likeDat<-paste(particles$M,obsData[i,2],sep=",")
+    weights = lapply(likeDat,dataLik)
+    weights<-as.vector(unlist(weights))
     
-    swP=sum(w)
-    w=w/swP
+    swP=sum(weights)
+    weights=weights/swP
     
-    w<-log(w)-max(log(w))
-    w<-(exp(w))
+    weights<-log(weights)-max(log(weights))
+    weights<-(exp(weights))
     
-    ll = ll + log(mean(w))
-    w[is.na(w)] <- 1e-200##only keep in if needed
-    rows = sample(1:n, n, replace = TRUE, prob = w)
-    xmat = xmat[rows, ]
+    ll = ll + log(mean(weights))
+    #weights[is.na(weights)] <- 1e-200##only keep in if needed
+    rows = sample(1:n, n, replace = TRUE, prob = weights)
+    particles = particles[rows, ]
   }
   
   ll
