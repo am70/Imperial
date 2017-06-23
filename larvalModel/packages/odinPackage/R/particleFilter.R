f0ca294ed30f5e8645b865c0267caa311feeced9
am70 <aaron.morris10@imperial.ac.uk>
2017-06-23 11:26:56 +0100

    minor fixes to particle filter

--- larvalModel/packages/odinPackage/R/particleFilter.R
+++ larvalModel/packages/odinPackage/R/particleFilter.R
@@ -1,6 +1,13 @@
 
+##################################################################################################################################################
+#                                                                                                                                                #
+#                                                                                                                                                #
+#                                                    particle Filter functions                                                                   #
+#                                                                                                                                                #
+#                                                                                                                                                #
+##################################################################################################################################################
 
-# initial state sampler
+# initial state sampler, samples random initial states
 iState <- function(N,t0)
 {
   mat=cbind(rpois(N,177),rpois(N,8),rpois(N,1),rpois(N,7))
@@ -17,20 +24,35 @@ dataLikFunc <- function(input)
 }
 
 
-## Log-Prior 
-lprior <- function(parms) {
-  uoEprior<-dnorm(parms[1],mean=0.035,sd=0.00485,log=T)
-  uoLprior<-dnorm(parms[2],mean=0.035,sd=0.00485,log=T)
-  uPprior<-dnorm(parms[3],mean=0.25,sd=0.0357,log=T)
-  Yprior<-dnorm(parms[4],mean=13.06,sd=3.53,log=T)
-  sfprior<-dunif(parms[6],min=0,max=100,log=T)
-  p0prior<-dnorm(parms[7],mean=0.5,sd=0.03,log=T)
-  priorSum<-as.vector(uoEprior+uoLprior+uPprior+Yprior+sfprior+p0prior)
-  return(priorSum)
+##model step function - runs model in steps taking the likelihood of the subsequent observed data point
+modStep3<-function(weightInput){
+  #parse input data
+  dataInput<-read.table(text = weightInput, sep = ",", colClasses = "numeric")
+  initialState<-as.numeric(dataInput[,c(1:4)])
+  currentTime<-as.numeric(dataInput[,c(5)]*10)#*10 as model is running in discrete time steps
+  nextTime<-as.numeric(dataInput[,c(6)]*10)
+  pr<-as.numeric(dataInput[,c(7:12)])
+  #initialise model
+  params<-mosParamsP(E0=initialState[1],L0=initialState[2],P0=initialState[3],M0=initialState[4],
+                     uoE=pr[1],uoL=pr[2],uP=pr[3],Y=pr[4],n=pr[5],sf=pr[6])
+  #run model between two discrete time periods and return results
+  modR <- larvalModP(user=params)
+  simDat <- as.data.frame(modR$run(seq(currentTime, nextTime, length.out=nextTime-currentTime)))
+  res2<-(simDat[simDat$step == nextTime,])
+  return(as.data.frame(rbind(res2[,c(8:11)])))
 }
 
 
-###Particle Filter
+
+##################################################################################################################################################
+#                                                                                                                                                #
+#                                                                                                                                                #
+#                                                    particle Filter                                                                             #
+#                                                                                                                                                #
+#                                                                                                                                                #
+##################################################################################################################################################
+
+
 pFilt <- function (n, iState, t0, stepFun, dataLik, obsData,prms) 
 {
   times = c(obsData$time)
@@ -49,12 +71,10 @@ pFilt <- function (n, iState, t0, stepFun, dataLik, obsData,prms)
 
     ll = ll + mean(weights)
   
+    #normalise weights
     swP=sum(weights)
-    
     weights=weights/swP
-    
     weights<-(weights)-max((weights))
-    
     weights<-exp(weights)
     
     weights[is.na(weights)] <- 1e-4##only keep in if needed
@@ -68,7 +88,5 @@ pFilt <- function (n, iState, t0, stepFun, dataLik, obsData,prms)
 }
 
 
-init.state <-
-  c(E = 177,L = 8,P = 1,M = 7)
 
 
