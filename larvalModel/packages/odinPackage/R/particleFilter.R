74f133c6792b631992a2da2cb13a74d82b1b09ee
am70 <aaron.morris10@imperial.ac.uk>
2017-06-27 14:45:16 +0100

    updates

--- larvalModel/packages/odinPackage/R/particleFilter.R
+++ larvalModel/packages/odinPackage/R/particleFilter.R
@@ -15,13 +15,11 @@ iState <- function(N,t0)
   mat
 }
 
-##negative binomial - gamma poisson (mixture) distribution #!needs to be changed to lgamma
-nBgP2<-function(x,n,p,log=F){
-if (log=F)(gamma(x+n)/(gamma(n)*factorial(x)))*(p^n)*(1-p)^x
-  else log((gamma(x+n)/(gamma(n)*factorial(x)))*(p^n)*(1-p)^x)
+##negative binomial - gamma poisson (mixture) distribution
+nBgP<-function(x,n,p,log=F){
+  if(log==T)lgamma(x+n)-(lgamma(n)+lfactorial(x))+log(p^n)+log((1-p)^x)
+  else exp(lgamma(x+n)-(lgamma(n)+lfactorial(x))+log(p^n)+log((1-p)^x))
 }
-
-
 
 
 #likelihood function
@@ -30,27 +28,27 @@ dataLikFunc <- function(input)
   dataInput<-read.table(text = input, sep = ",", colClasses = "numeric")
   #dataInput[1]=Simulated data point
   #dataInput[2]=Observed data point
-  ll = dnbinom(as.numeric(round(dataInput[1],0)), as.numeric(dataInput[2]+1), prob=as.numeric(dataInput[3]), log = T)
+  ll = nBgP(as.numeric(round(dataInput[1],0)), as.numeric(dataInput[2]+1), prob=as.numeric(dataInput[3]), log = T)
   return (ll)
 }
 
 
 
-##model step function - runs model in steps taking the likelihood of the subsequent observed data point
+##model step function - runs model in steps using Odin, returning the model state at a subsequent time point
 modStep3<-function(weightInput){
   #parse input data
   dataInput<-read.table(text = weightInput, sep = ",", colClasses = "numeric")
-  initialState<-as.numeric(dataInput[,c(1:4)])
-  currentTime<-as.numeric(dataInput[,c(5)])
-  nextTime<-as.numeric(dataInput[,c(6)])
-  pr<-as.numeric(dataInput[,c(7:12)])
+  initialState<-as.numeric(dataInput[,c(1:4)])#state of E, L, P and M
+  currentTime<-as.numeric(dataInput[,c(5)])#time in model to run from
+  nextTime<-as.numeric(dataInput[,c(6)])#time in model to run to
+  pr<-as.numeric(dataInput[,c(7:12)])#parameters
   #initialise model
   params<-mosParamsP(E0=initialState[1],L0=initialState[2],P0=initialState[3],M0=initialState[4],
                      uoE=pr[1],uoL=pr[2],uP=pr[3],Y=pr[4],n=pr[5],sf=pr[6])
   #run model between two discrete time periods and return results
-  modR <- larvalModP(user=params)
+  modR <- larvalModP(user=params)#run model
   simDat <- as.data.frame(modR$run(seq(currentTime, nextTime, length.out=nextTime-currentTime)))
-  res2<-(simDat[simDat$step == nextTime,])
+  res2<-(simDat[simDat$step == nextTime,])#return model state
   return(as.data.frame(rbind(res2[,c(8:11)])))
 }
 
@@ -74,24 +72,18 @@ pFilt <- function (n, iState, t0, stepFun, likeFunc, obsData,prms)
   for (i in 1:length(times[-length(times)])) {
     wp<-paste(particles[,1],particles[,2],particles[,3], particles[,4],times[i],
               times[i + 1],prms[1],prms[2],prms[3],prms[4],prms[5],prms[6],sep=",")
-    
     particlesTemp = parLapply(NULL,wp,stepFun) #use NULL for dide cluster, cl for local
     particles<-data.frame(t(sapply(particlesTemp, `[`)))
-    
     likeDat<-paste(particles$M,obsData[i+1,2],prms[7],sep=",")
     weights = parLapply(NULL,likeDat,likeFunc)
     weights<-as.vector(unlist(weights))
-
     ll = ll + mean(weights)
-  
     #normalise weights
+    weights<-exp(weights)
     swP=sum(weights)
     weights=weights/swP
-    weights<-(weights)-max((weights))
-    weights<-exp(weights)
-    
-    weights[is.na(weights)] <- 1e-4##only keep in if needed
-    
+    weights<-(weights)-max(weights)
+    weights<-exp(weights)*0.9
     rows = sample(1:n, n, replace = TRUE, prob = weights)
     particles = particles[rows, ]
   }
@@ -100,4 +92,6 @@ pFilt <- function (n, iState, t0, stepFun, likeFunc, obsData,prms)
   
 }
 
+# llx<-ll[,sample(1:length(tail(ll,1)),1,prob=exp(head(tail(ll,2),1)))]
+# sum(llx[-1])
 
