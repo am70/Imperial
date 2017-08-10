31135d883adef81e9496efa2b5ec1865c2c4d986
am70 <aaron.morris10@imperial.ac.uk>
2017-08-10 12:02:17 +0100

    updates

--- larvalModel/packages/odinPackage/startup.R
+++ larvalModel/packages/odinPackage/startup.R
@@ -16,7 +16,7 @@ library(coda)
 #                                                     load in data                                                                      #
 #                                                                                                                                       #
 #########################################################################################################################################
-delta<-0.1#discrete time period
+delta<-0.25#discrete time period
 
 #simDat<-read.csv("C:\\simDat.csv",sep=" ")
 ##load in Garki rainfall data NOT YET VILLAGE SPECIFIC
@@ -24,7 +24,7 @@ rainfall<-read.csv("Q:\\Imperial\\larvalModel\\Data\\meteoFUP1.csv",head=F)
 colnames(rainfall)<-c("rainfall","date")
 rainfall$date<-dmy(rainfall$date)
 #limit data to one year
-rainfall<-subset(rainfall, date >= as.Date("1971-04-27") & date <= as.Date("1973-01-01"))
+rainfall<-subset(rainfall, date >= as.Date("1971-01-01") & date <= as.Date("1973-04-27"))
 rainfall$time<-(1:nrow(rainfall))
 rFx<-rainfall[rep(seq_len(nrow(rainfall)), each=1/delta),]
 rFx<-rFx[,1]
@@ -33,7 +33,7 @@ rainfall2<-read.csv("Q:\\Imperial\\larvalModel\\Data\\meteoFUP2.csv",head=F)
 colnames(rainfall2)<-c("rainfall","date")
 rainfall2$date<-dmy(rainfall2$date)
 #limit data to one year
-rainfall2<-subset(rainfall2, date >= as.Date("1971-04-27") & date <= as.Date("1973-01-01"))
+rainfall2<-subset(rainfall2, date >= as.Date("1971-01-01") & date <= as.Date("1973-04-27"))
 rainfall2$time<-(1:nrow(rainfall2))
 rFx2<-rainfall[rep(seq_len(nrow(rainfall)), each=1/delta),]
 rFx2<-rFx2[,1]
@@ -60,7 +60,7 @@ plot(garkiObs$time,garkiObs$M,main=i)
 colnames(garkiObs)<-c("time",i)
 assign(paste0("garkiObs", i),garkiObs)
 }
-garkiObsX<-Reduce(function(...) merge(..., all=TRUE), list(garkiObs101, garkiObs104,garkiObs111,garkiObs113))
+garkiObsX<-Reduce(function(...) merge(..., all=TRUE), list(garkiObs101, garkiObs104,garkiObs219,garkiObs220))
 
 
 
@@ -81,7 +81,7 @@ clusterEvalQ(cl, library("odinPackage"))
 clusterEvalQ(cl, library("lhs"))
 clusterEvalQ(cl, library("VGAM"))
 clusterEvalQ(cl, library("deSolve"))
-clusterExport(cl, c("rFx","delta","garkiObs","nBgP"), envir=environment())
+clusterExport(cl, c("rFx","rFx2","delta","garkiObs","nBgP"), envir=environment())
 
 
 ##################################################################################################################################################
@@ -93,14 +93,47 @@ clusterExport(cl, c("rFx","delta","garkiObs","nBgP"), envir=environment())
 ##################################################################################################################################################
 
 #set.seed(44)
-system.time(runX200z5 <- mcmcSampler(initParams = c(uoE=0.03311636,uoL=0.035,uP=0.25,Y=15.30829479,n=5,p0=0.49791206,o=20,Fp=0.5,
-                                                    o2=1,sf1=10,sf2=10,sf3=10,sf4=10)
-                                     ,nburn=1
+system.time(runX200z5 <- mcmcSampler(initParams = c(uoE=0.03526661,uoL=0.03776644,uP=0.25516500,Y=16.84519894,n=20,p0=0.51322692,o=0.01,Fp=0.97126420,
+                                                    o2=1.29593424,sf1=7.31392687,sf2=2.35775867,sf3=10.01558351,sf4=11.56955667)
+                                     ,nburn=1000
                                      ,monitoring=2
-                                     , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0.1,0.01,1,0.01,0.01,0.1,0.1,0.1,0.1))
+                                     , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0.1,0.01,0.01,0.01,0.01,0.1,0.1,0.1,0.1))
                                      , randInit = F
                                      , particles=96
-                                     , niter = 10))
+                                     , niter = 10000))
+
+
+covar=matrix(c(.001,0,0,0,0,0,0,0,0,0,0,0,0,
+               0,.001,0,0,0,0,0,0,0,0,0,0,0,
+               0,0,.001,0,0,0,0,0,0,0,0,0,0,
+               0,0,0,.001,0,0,0,0,0,0,0,0,0,
+               0,0,0,0,.001,0,0,0,0,0,0,0,0,
+               0,0,0,0,0,.001,0,0,0,0,0,0,0,
+               0,0,0,0,0,0,.001,0,0,0,0,0,0,
+               0,0,0,0,0,0,0,.001,0,0,0,0,0,
+               0,0,0,0,0,0,0,0,.001,0,0,0,0,
+               0,0,0,0,0,0,0,0,0,.001,0,0,0,
+               0,0,0,0,0,0,0,0,0,0,.001,0,0,
+               0,0,0,0,0,0,0,0,0,0,0,.001,0,
+               0,0,0,0,0,0,0,0,0,0,0,0,.001),13,13)
+
+
+
+
+#set.seed(44)
+system.time(runX200z5 <- mcmcSampler(initParams = c(uoE=0.02783780,uoL=0.04538763,uP=0.25001641,Y=16.86354430,n=60,p0=0.51010071,o=0.09217603,Fp=0.97413706,
+                                                    o2=0.91439265,sf1=0.76201783,sf2=0.15444917,sf3=0.85442365,sf4=1.10734896)
+                                     ,nburn=1000
+                                     ,monitoring=2
+                                     , proposer = multiv.proposer(covar)
+                                     ,sdProps=c(0.1,0.1,0.1,1,0,0.1,0.1,0.1,0.1,1,1,1,1)
+                                     , randInit = F
+                                     ,adaptiveMCMC = T
+                                     , startAdapt = 150
+                                     , particles=126
+                                     ,acceptanceRate = 0.3
+                                     , niter = 10000))
+
 
 
 
@@ -115,7 +148,7 @@ meds<-function(x){cbind(median(x[,1]),median(x[,2]),median(x[,3]),median(x[,4]),
 testParams = c(uoE=0.031,uoL=0.035,uP=0.26,Y=17.13,n=25,p0=0.58,Fp=0.3,o=0.03,sf=4.05)
 res4<-NULL
 system.time(for (i in 1:50){
-  ss<-pFilt(35,iState,modStep3,dataLikFunc,garkiObs101,pr=testParams)+ lprior(testParams)
+  ss<-pFilt(35,iState,modStep3,dataLikFunc,garkiObs101,pr=testParams) + lprior(testParams)
   res4<-rbind(ss,res4)
   print(ss)
 })
@@ -158,23 +191,23 @@ obj <- didehpc::queue_didehpc(ctx,didehpc::didehpc_config(cluster="mrc",home="//
 #                                                                                                                                       #
 #########################################################################################################################################
 
-runX1_1x1 <- obj$enqueue(mcmcSampler(initParams = c(uoE=0.03311636,uoL=0.035,uP=0.25,Y=15.30829479,n=10,p0=0.49791206,o=5,Fp=0.5,
-                                                  o2=1,sf1=10,sf2=10,sf3=10,sf4=10)
+runX1_1x1L <- obj$enqueue(mcmcSampler(initParams = c(uoE=0.03386983,uoL=0.03652054,uP=0.25801683,Y=14.61421801,n=10,p0=0.51241423,o=0.45443296,Fp=0.99098142,
+                                                  o2=1.36693761,sf1=6.09204989,sf2=2.49848543,sf3=9.00862475,sf4=11.17703509)
                                    ,nburn=5000
                                    ,monitoring=2
-                                   , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0,0.1,1,0.01,0.01,0.5,0.5,0.5,0.5))
+                                   , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0.1,0.1,0.1,0.01,0.01,0.5,0.5,0.5,0.5))
                                    ,particles = 112
                                  , randInit = F
-                                 , niter = 100000),name="pMMH nx")
+                                 , niter = 1000000),name="pMMH nx1M")
 
-runX1_1xx1 <- obj$enqueue(mcmcSampler(initParams = c(uoE=0.03311636,uoL=0.035,uP=0.25,Y=15.30829479,n=10,p0=0.49791206,o=5,Fp=0.5,
-                                                   o2=1,sf1=10,sf2=10,sf3=10,sf4=10)
+runX1_1xx1 <- obj$enqueue(mcmcSampler(initParams = c(uoE=0.03386983,uoL=0.03652054,uP=0.25801683,Y=14.61421801,n=10,p0=0.51241423,o=0.45443296,Fp=0.99098142,
+                                                     o2=1.36693761,sf1=6.09204989,sf2=2.49848543,sf3=9.00862475,sf4=11.17703509)
                                     ,nburn=1000
                                     ,monitoring=2
-                                    , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0,0.1,1,0.01,0.01,0.5,0.5,0.5,0.5))
+                                    , proposer = sequential.proposer(sdProps=c(0.001,0.001,0.01,0.1,0,0.1,0.1,0.01,0.01,0.5,0.5,0.5,0.5))
                                     ,particles = 96
                                     , randInit = F
-                                    , niter = 10000),name="pMMH nx")
+                                    , niter = 1000000),name="pMMH n10_1M")
 
 
 
